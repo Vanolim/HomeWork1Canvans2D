f4f5e592cae3d052caa80cca5883c91bac9ee9bd
Иван Селютин <legavana@mail.ru>
2021-01-16 22:06:05 +0300

    creaty

---
+++ Assets/Scripts/ChangeSliderFromHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeSliderFromHealth : MonoBehaviour
+{
+    [SerializeField] private HealthChange _healthValue;
+
+    private Slider _slider;
+
+    private void Start()
+    {
+        _slider = gameObject.GetComponent<Slider>();
+    }
+
+    private void OnEnable()
+    {
+        _healthValue.HealthChanged += ChangeValue;
+    }
+
+    private void OnDisable()
+    {
+        _healthValue.HealthChanged -= ChangeValue;
+    }
+
+    private void ChangeValue(float value)
+    {
+        _slider.value = value;
+    }
+}
---
+++ Assets/Scripts/HealthChange.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthChange : MonoBehaviour
+{
+    private float _variableValue = 10f;
+    private float _maxHealth = 100f;
+    private float _minHealth = 0f;
+    private float _healthValue;
+
+    public event UnityAction<float> HealthChanged;
+    
+    private void ChangeValue(float value)
+    {
+        float valueChange = _healthValue + value;
+
+        if (valueChange >= _minHealth && valueChange <= _maxHealth)
+        {
+            _healthValue = valueChange;
+            HealthChanged.Invoke(_healthValue);
+        }
+    }
+
+    public void TakeDamage()
+    {
+        ChangeValue(_variableValue * -1);
+    }
+
+    public void Cure()
+    {
+        ChangeValue(_variableValue);
+    }
+}
